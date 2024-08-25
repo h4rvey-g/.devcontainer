9126eab0967067a3b89c1786c26f01ade4e65262
babaolanqiu <babaolanqiu@gmail.com>
2024-08-25 14:15:39 +0800

    chore: Update postCreateCommand.sh script

--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -1,7 +1,7 @@
 pip install ipykernel ipywidgets
 
 if [ -f requirements.txt* ]; then
-    pip install -r requirements.txt
+  pip install -r requirements.txt
 else pip install pandas numpy matplotlib seaborn scikit-learn; fi
 # fix permission issue
 sudo chmod 777 /usr/local/lib/R/site-library/_cache/
@@ -10,7 +10,6 @@ echo 'if (interactive() && Sys.getenv("RSTUDIO") == "") {
   source(file.path(Sys.getenv(if (.Platform$OS.type == "windows") "USERPROFILE" else "HOME"), ".vscode-R", "init.R"))
 }' >>~/.Rprofile
 # add library(tidyverse) and library(qs) to .Rprofile
-echo 'library(tidyverse)
-library(qs)' >>~/.Rprofile
+echo 'options(defaultPackages=c(getOption("defaultPackages"), "tidyverse", "targets", "qs"' >>~/.Rprofile
 # add github hosts
 curl https://gitlab.com/ineo6/hosts/-/raw/master/next-hosts | sudo tee -a /etc/hosts
